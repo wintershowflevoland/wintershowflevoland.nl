c77af2271cb17e1f8a7bc2a4f01ef010c3e912fe
jcklerk <justin@klerk.nl>
2024-12-01 15:45:21 +0100

    working build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: "export",
+    distDir: "out",
     images: { unoptimized: true }
 };
 
--- src/app/nieuws/[slug]/page.tsx
+++ src/app/nieuws/[slug]/page.tsx
@@ -1,7 +1,5 @@
-import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getAllPosts, getPostBySlug } from "../../../lib/api";
-import { CMS_NAME } from "../../../lib/constants";
 import markdownToHtml from "../../../lib/markdownToHtml";
 import Alert from "../../_components/alert";
 import Container from "../../_components/container";
@@ -9,61 +7,63 @@ import Header from "../../_components/header";
 import { PostBody } from "../../_components/post-body";
 import { PostHeader } from "../../_components/post-header";
 
-export default async function Post({ params }: Params) {
-  const post = getPostBySlug(params.slug);
+export default async function Nieuws({ params }: Params) {
+	const slug = (await params).slug;
+	const post = getPostBySlug(slug);
 
-  if (!post) {
-    return notFound();
-  }
+	if (!post) {
+		return notFound();
+	}
 
-  const content = await markdownToHtml(post.content || "");
+	const content = await markdownToHtml(post.content || "");
 
-  return (
-    <main>
-      <Alert preview={post.preview} />
-      <Container>
-        <Header />
-        <article className="mb-32">
-          <PostHeader
-            title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-          />
-          <PostBody content={content} />
-        </article>
-      </Container>
-    </main>
-  );
+	return (
+		<main>
+			<Alert preview={post.preview} />
+			<Container>
+				<Header />
+				<article className="mb-32">
+					<PostHeader
+						title={post.title}
+						coverImage={post.coverImage}
+						date={post.date}
+						author={post.author}
+					/>
+					<PostBody content={content} />
+				</article>
+			</Container>
+		</main>
+	);
 }
 
 type Params = {
-  params: {
-    slug: string;
-  };
+	params: Promise<{
+		slug: string;
+	}>;
 };
 
-export function generateMetadata({ params }: Params): Metadata {
-  const post = getPostBySlug(params.slug);
+// export async function generateMetadata({ params }: Params): Promise<Metadata> {
+// 	const slug = (await params).slug;
+// 	const post = getPostBySlug(slug);
 
-  if (!post) {
-    return notFound();
-  }
+// 	if (!post) {
+// 		return notFound();
+// 	}
 
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+// 	const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
 
-  return {
-    openGraph: {
-      title,
-      images: [post.ogImage.url],
-    },
-  };
-}
+// 	return {
+// 		openGraph: {
+// 			title,
+// 			images: [post.ogImage.url],
+// 		},
+// 	};
+// }
 
-export async function generateStaticParams() {
-  const posts = getAllPosts();
+export function generateStaticParams() {
+	const posts = getAllPosts();
 
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
+	return posts.map((post) => ({
+		slug: post.slug,
+	}));
 }
